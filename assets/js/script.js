c1192fe8181c2dfe7c5476bd7fc0e1293b3dc75d
Niall Carroll <niallpcarroll@gmail.com>
2024-06-04 12:55:44 +0000

    Add initial questions in JS. Change background colour CSS

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+const questions = [{
+    question: "Where was Johann Sebastian Bach born?",
+    choices: ["Eisenach", "Leipzig", "Köthen", "Arnstadt"],
+    correct: 0
+}, 
+{
+    question: "Which Baroque composer was known as 'The Red Priest'?",
+    choices: ["Telemann", "Purcell", "Scarlatti", "Vivaldi"],
+    correct: 3
+},
+{
+    question: "Which of these is an opera by W.A. Mozart?",
+    choices: ["Cosi fan tutte", "Dialogues des Carmélites", "Madama Butterfly", "La traviata"],
+    correct: 0
+},
+{
+    question: "If a piece of music is marked 'legato', how should it be played?",
+    choices: ["Quickly", "Smoothly", "Loudly", "Detached"],
+    correct: 1
+},
+{
+    question: "A string quartet requires how many players?",
+    choices: ["3", "4", "5", "6"],
+    correct: 1
+},
+{
+    question: "Irish composer John Field is widely credited as the creator of which musical form?",
+    choices: ["Sonata", "Minuet", "Nocturne", "Symphony"],
+    correct: 2
+},
+{
+    question: "Ludwig van Beethoven only wrote one opera. What is it called?",
+    choices: ["The Magic Flute", "Scipione", "Fidelio", "Rigoletto"],
+    correct: 2
+},
+{
+    question: "Which of these was NOT a French composer?",
+    choices: ["Charpentier", "Berlioz", "Poulenc", "Liszt"],
+    correct: 3
+},
+{
+    question: "Who composed a symphony nicknamed 'Surprise'?",
+    choices: ["Schubert", "Schumann", "Haydn", "Wagner"],
+    correct: 2
+},
+{
+    question: "How many keys are there on a standard piano?",
+    choices: ["87", "88", "89", "90"],
+    correct: 1
+},
+{
+    question: "In which city was Charles Villiers Stanford born?",
+    choices: ["Dublin", "London", "Bordeaux", "Edinburgh"],
+    correct: 0
+},
+{
+    question: "The famous organist and composer Charles-Marie Widor was organist of which Parisian church?",
+    choices: ["Notre Dame Cathedral", "La Madeleine", "Sainte-Clotilde", "Saint-Sulpice"],
+    correct: 3
+},
+{
+    question: "Which of these musical periods came first?",
+    choices: ["Baroque", "Renaissance", "Romantic", "Classical"],
+    correct: 1
+},
+
+]
